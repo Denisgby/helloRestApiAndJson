e9cfa1ce9527216a2232d031e3a9978518c3d3cd
Denisgby <denis.gby@gmail.com>
2021-12-06 00:42:36 +0300

    da

--- main.js
+++ main.js
@@ -1,28 +1,29 @@
 fetch('https://mdn.github.io/learning-area/javascript/oojs/json/superheroes.json')
-.then((res) => res.json())
-.then((data)=> setHeroes(data)) ;
+    .then((res) => res.json())
+    .then((data) => setHeroes(data));
 
-function setHeroes({squadName,homeTown,formed,members}){
+function setHeroes({ squadName, homeTown, formed, members }) {
     document.body.insertAdjacentHTML(
         'afterbegin',
         `
         <h1>${squadName}</h1>
         <h2>${homeTown} // Formed: ${formed} </h2>
- <div class="heroes"></div>
-        ` 
+ <div class="heroes">${setMembers(members)}</div>
+        `
     );
-}  
+}
 
-function setMembers(members){
+function setMembers(members) {
     return members.map(
-        (hero) =>`
+        (hero) => `
         <h3>${hero.name}</h3>
         <p>Secret indentity: ${hero.secretIdentity}</p>
         <p>Age: ${hero.age}</p>
         <p>Superpowers: </p>
         <ul>
-        ${hero.powers.map((power) => `<li>${power}`)}
+           ${hero.powers.map((power) => `<li>${power}</li>`).join(' ')}
         </ul>
         `
-    
+    )
+    .join(' ') ;
 }
