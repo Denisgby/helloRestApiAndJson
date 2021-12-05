4cdb660b591c88b0bad9b1794bc53046c6016486
Denisgby <denis.gby@gmail.com>
2021-12-06 00:23:58 +0300

    ayf

--- main.js
+++ main.js
@@ -1,12 +1,13 @@
 fetch('https://mdn.github.io/learning-area/javascript/oojs/json/superheroes.json')
 .then((res) => res.json())
-.then(data=> console.log(data)) ;
+.then((data)=> setHeroes(data)) ;
 
 function setHeroes({squadName,homeTown,formed,members}){
     document.body.insertAdjacentHTML(
         'afterbegin',
         `
         <h1>${squadName}</h1>
-    ` 
+        <h2>${homeTown}</h2>
+        ` 
     );
-}
+}  
